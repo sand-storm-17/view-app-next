6efda5642e88356139b996b09859fd41c76bce9e
sand-storm-17 <sandeeepkumaar.0034@gmail.com>
2024-08-28 21:11:26 +0530

    removing prisma imports

--- app/api/youtubers/route.ts
+++ app/api/youtubers/route.ts
@@ -1,22 +1,22 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export async function GET() {
-  try {
-    const youtubers = await prisma.youTuber.findMany({
-      orderBy: {
-        subscriberCount: 'desc', 
-      },
-      select: {id:true, rank:true, subscriberCount:true, channelName:true}
-    });
-    return NextResponse.json({youtubers});
-  } catch (error) {
-    console.error('Error fetching YouTubers:', error);
-    return NextResponse.error();
-  } finally {
-    await prisma.$disconnect();
-  }
+  // try {
+  //   const youtubers = await prisma.youTuber.findMany({
+  //     orderBy: {
+  //       subscriberCount: 'desc', 
+  //     },
+  //     select: {id:true, rank:true, subscriberCount:true, channelName:true}
+  //   });
+  //   return NextResponse.json({youtubers});
+  // } catch (error) {
+  //   console.error('Error fetching YouTubers:', error);
+  //   return NextResponse.error();
+  // } finally {
+  //   await prisma.$disconnect();
+  // }
 }
 
